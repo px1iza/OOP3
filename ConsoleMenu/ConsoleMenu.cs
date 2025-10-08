85c63251d7ae6f32619e88c7c7218032b58f52ad
Liza <rabirokhliza@gmail.com>
2025-10-08 21:08:18 +0300

    lab3.1

--- ConsoleMenu/ConsoleMenu.cs
+++ ConsoleMenu/ConsoleMenu.cs
@@ -6,67 +6,30 @@ namespace ConsoleMenu
 {
     internal class Program
     {
-        static Human[] InputPeople()
-        {
-            Console.Write("Скільки осіб ви хочете додати? ");
-            int n = int.Parse(Console.ReadLine()!);
-            Human[] people = new Human[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine($"\n--- Введення даних для особи №{i + 1} ---");
-                Console.WriteLine("Оберіть тип: 1 - Студент, 2 - Бібліотекар, 3 - Розробник");
-                string type = Console.ReadLine()!;
-
-                Console.Write("Ім'я: ");
-                string firstName = Console.ReadLine();
-                Console.Write("Прізвище: ");
-                string lastName = Console.ReadLine();
-
-                if (type == "1")
-                {
-                    Console.Write("Зріст (см): ");
-                    int height = int.Parse(Console.ReadLine());
-                    Console.Write("Вага (кг): ");
-                    int weight = int.Parse(Console.ReadLine());
-                    Console.Write("Студентський квиток (напр. KB123456): ");
-                    string studentId = Console.ReadLine();
-                    Console.Write("Серія паспорта (2 літери): ");
-                    string passportSeries = Console.ReadLine();
-                    Console.Write("Номер паспорта (6 цифр): ");
-                    int passportNumber = int.Parse(Console.ReadLine());
-
-                    people[i] = new Student(firstName, lastName, height, weight, studentId, new Passport(passportSeries, passportNumber));
-                }
-                else if (type == "2")
-                {
-                    people[i] = new Librarian(firstName, lastName);
-                }
-                else
-                {
-                    people[i] = new SoftwareDeveloper(firstName, lastName);
-                }
-            }
-            return people;
-        }
-
         static void Main()
         {
-            Human[] peopleFromInput = InputPeople();
+            Human[] people = new Human[]
+            {
+                new Student("Олександр", "Петренко", 180, 70, "KB123456", new Passport("AB", 987654)),
+                new Student("Марія", "Коваль", 165, 55, "KB789012", new Passport("AC", 345678)),
+                new Librarian("Світлана", "Мельник"),
+                new SoftwareDeveloper("Євген", "Романюк"),
+                new Student("Іван", "Сидоренко", 175, 65, "KB456789", new Passport("AD", 111222)),
+                new Student("Наталія", "Шевченко", 160, 50, "KB333444", new Passport("AE", 222333)),
+                new Student("Дмитро", "Гриценко", 190, 80, "KB555666", new Passport("AF", 333444))
+            };
 
-            DataStream stream = new DataStream(peopleFromInput);
+            DataStream stream = new DataStream(people);
             string fileName = "people.txt";
             stream.WriteToFile(fileName);
-            Console.WriteLine($"\nДані успішно записано у файл '{fileName}'!");
 
-            Console.WriteLine("\n--- Читання даних з файлу ---");
-            Human[] peopleFromFile = stream.ReadFromFile(fileName);
-            Display(peopleFromFile);
+            Console.WriteLine($"Дані записано у файл '{fileName}'!");
+            Console.WriteLine($"Кількість студентів з ідеальною вагою: {stream.ReadFromFile(fileName)}");
+            Console.WriteLine("\nСписок усіх осіб:");
+            Display(people);
 
-            Console.WriteLine($"\nКількість студентів з ідеальною вагою: {stream.CountIdealWeightStudents(fileName)}");
-
-            Console.WriteLine("\n--- Демонстрація навички ---");
-            foreach (var person in peopleFromFile)
+            Console.WriteLine("\nДемонстрація навички:");
+            foreach (var person in people)
             {
                 if (person is ISkill skilledPerson)
                 {
